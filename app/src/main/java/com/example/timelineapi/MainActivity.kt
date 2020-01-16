5aaf3ffcff76ce069266a820b74feea70f6862a6
MtDeity <tagosakufrancokamiyama@yahoo.co.jp>
2020-01-16 15:36:37 +0900

    例外処理追加
    登録失敗理由は未実装

--- app/src/main/java/com/example/timelineapi/MainActivity.kt
+++ app/src/main/java/com/example/timelineapi/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.timelineapi
 
 import android.content.Intent
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
@@ -41,8 +42,13 @@ class MainActivity : AppCompatActivity() {
         receiver.execute(emailStr, passwordStr, passwordConfirmationStr, nameStr, bioStr)
     }
 
+
+
     private inner class SignUpReceiver() : AsyncTask<String, String, String>() {
         override fun doInBackground(vararg params: String): String {
+            var result= ""
+            var urlConnection: HttpURLConnection? = null
+
             val paramsValue = JSONObject()
             val email = params[0]
             val password = params[1]
@@ -59,31 +65,57 @@ class MainActivity : AppCompatActivity() {
 
             val urlStr = "https://teachapi.herokuapp.com/sign_up"
             val url = URL(urlStr)
-            val urlConnection = url.openConnection() as HttpURLConnection
-            urlConnection.requestMethod = "POST"
-            urlConnection.addRequestProperty("Content-Type", "application/json; charset=UTF-8")
-            urlConnection.doOutput = true
-            urlConnection.doInput  = true
-            urlConnection.connect()
-            val outputStream = PrintStream(urlConnection.outputStream)
-            outputStream.print(bodyParameter)
-            outputStream.close()
 
-            val inputStream = urlConnection.inputStream
-            val result = is2String(inputStream)
-            urlConnection.disconnect()
+            try{
+                urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 100000
+                urlConnection.readTimeout = 100000
+                urlConnection.requestMethod = "POST"
+                urlConnection.addRequestProperty("Content-Type", "application/json; charset=UTF-8")
+                urlConnection.doOutput = true
+                urlConnection.doInput = true
+                urlConnection.connect()
+                val outputStream = PrintStream(urlConnection.outputStream)
+                outputStream.print(bodyParameter)
+                outputStream.close()
 
-            return result
+                val inputStream = urlConnection.inputStream
+                result = is2String(inputStream)
+            }catch(e: Exception){
+                e.printStackTrace()
+            }finally {
+                urlConnection?.disconnect()
+                return result
+            }
         }
 
         override fun onPostExecute(result: String) {
-            val rootJSON = JSONObject(result)
+            //空白 -> 通信エラー
+            //Validation failed -> 正しく入力されていません
+            //そのemailは登録されている -> そのメールアドレスはすでに登録されています
+            //成功 -> 登録完了
 
-            responseName = rootJSON.getString("name")
-            responseBio = rootJSON.getString("bio")
-            responseEmail = rootJSON.getString("email")
+            lateinit var status: String
+            var responseName = "name初期値"
+            var responseBio = "bio初期値"
+            var responseEmail = "email初期値"
+
+            if (result == "") {
+                status = "登録失敗"
+            } else {
+                val rootJSON = JSONObject(result)
+                if (rootJSON.has("id")) {
+                    status = "登録完了"
+                    responseName = rootJSON.getString("name")
+                    responseBio = rootJSON.getString("bio")
+                    responseEmail = rootJSON.getString("email")
+                } else {
+                    status = "入力エラー"
+                }
+            }
 
             val intent = Intent(applicationContext, SignedUpActivity::class.java)
+            intent.putExtra("status", status)
             intent.putExtra("name", responseName)
             intent.putExtra("bio", responseBio)
             intent.putExtra("email", responseEmail)
