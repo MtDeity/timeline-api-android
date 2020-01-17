389171b8cd0927ac69288560145b9897f788be81
MtDeity <tagosakufrancokamiyama@yahoo.co.jp>
2020-01-17 14:50:05 +0900

    ログイン後画面修正

--- app/src/main/java/com/example/timelineapi/SignedInActivity.kt
+++ app/src/main/java/com/example/timelineapi/SignedInActivity.kt
@@ -1,12 +1,24 @@
 package com.example.timelineapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SignedInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signed_in)
+
+        val title = findViewById<TextView>(R.id.tvSignedInTitle)
+        title.text = intent.getStringExtra("status")
+        
+//        val contents = File(filesDir, "token.txt").bufferedReader().useLines { lines ->
+//            lines.fold("") { working, line ->
+//                "$working\n$line"
+//            }
+//        }
+//        findViewById<TextView>(R.id.tvSignedUpTitle).text = contents
+
     }
 }
